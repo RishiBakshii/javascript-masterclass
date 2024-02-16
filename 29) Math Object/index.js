8c57d860af35bfe01e27cb571d490271dce6c423
rishibakshi <guitarguyrishi@gmail.com>
2024-02-16 15:21:39 +0530

    Add Math Object

---
+++ 29) Math Object/index.js
@@ -0,0 +1,12 @@
+let number=9.6
+
+console.log(Math.round(number));
+
+console.log(Math.ceil(number));
+
+console.log(Math.floor(number));
+
+console.log(Math.trunc(number));
+
+let random=Math.random()
+console.log(Math.round(random*100));
